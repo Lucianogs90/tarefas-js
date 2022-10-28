b87758efe0c284ecd3a984288e6c4c3444ae8c49
Lucianogs90 <lucianogs90@gmail.com>
2022-10-27 22:22:41 -0400

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,16 +4,26 @@
 
 // console.log(localStorage.getItem("nome"))
 
-const tarefas = [
-    { id: 1, descricao: 'Pagar a conta de Energia' },
-    { id: 2, descricao: 'Estudar para a prova' },
-    { id: 3, descricao: 'Fazer tarefa de BD' }
-]
+// const tarefas = [
+//     { id: 1, descricao: 'Pagar a conta de Energia' },
+//     { id: 2, descricao: 'Estudar para a prova' },
+//     { id: 3, descricao: 'Fazer tarefa de BD' }
+// ]
 
-localStorage.setItem("tarefas", JSON.stringify(tarefas))
+// localStorage.setItem("tarefas", JSON.stringify(tarefas))
 
-const listaDeTarefas = JSON.parse(localStorage.getItem("tarefas"))
+// const listaDeTarefas = JSON.parse(localStorage.getItem("tarefas"))
 
-console.log(typeof(listaDeTarefas))
+// console.log(typeof(listaDeTarefas))
 
-console.log(listaDeTarefas[0].descricao)
+// console.log(listaDeTarefas[0].descricao)
+
+const inputTarefa = document.querySelector('#tarefa')
+// console.log(inputTarefa.value)
+
+const formTarefa = document.querySelector('#form')
+
+formTarefa.addEventListener('submit', function(event){
+    event.preventDefault()
+    console.log(inputTarefa.value)
+})
